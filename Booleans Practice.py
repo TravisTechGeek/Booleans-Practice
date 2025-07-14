b15d32c4491bf8f07f7c0f877468270739f99f55
Travis Paul Pyle <travispyle@gmail.com>
2025-07-14 15:27:38 -0400

    Add files via upload

---
+++ Booleans Practice.py
@@ -0,0 +1,11 @@
+# Boolean Statements are either true or false
+# Opinions are neither true or false
+
+# Statement: The Earth revolves around the Sun.
+first_statement = "Yes" # this boolean statement can be determined to be true
+
+# Statement: The Moon is made of cheese.
+second_statement = "Yes" # this boolean statement can be determined to be false.
+
+# Statement: Chocolate ice cream tastes better than vanilla.
+third_statement = "No" # this statement is an opinion and is neither true or false
