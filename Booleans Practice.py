645fedb793edd6885c9556fa85f2d581bee8eb34
Travis Paul Pyle <travispyle@gmail.com>
2025-07-15 11:36:53 -0400

    Update Booleans Practice.py
    updated a wrong statement for line 36

--- Booleans Practice.py
+++ Booleans Practice.py
@@ -33,7 +33,7 @@ if not credits >= 120:
   print('You do not have enough credits to graduate.')
 
 if not gpa >= 2.0:
-  print('You do not meet either requirement to graduate!')
+  print('Your GPA is not high enough to graduate.')
 
 if not (credits >= 120) and not (gpa >= 2.0):
   print('You do not meet either requirement to graduate!')
